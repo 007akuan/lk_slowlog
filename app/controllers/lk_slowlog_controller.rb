29d0a28e4a047e1dc58c66413d430655c0414260
007akuan <akfdgg@163.com>
2020-12-05 12:23:07 +0800

    排除实例rm-m5eqyh7u824r4tfho下数据库ikcrm_production

--- app/controllers/lk_slowlog_controller.rb
+++ app/controllers/lk_slowlog_controller.rb
@@ -2,9 +2,11 @@ class LkSlowlogController < ApplicationController
   def index
     
     #@instance_ids = SlowlogDetail.select("instance_id").distinct.pluck(:instance_id)
-    @dbnames = SlowlogDetail.select("DBName").distinct.where("DBName<>'crm_prod_drds'").pluck(:DBName)
+    @dbnames = SlowlogDetail.select("DBName").distinct.where("DBName<>'crm_prod_drds' ").pluck(:DBName)
 
-    @slowlog_detail =SlowlogDetail.select("id,instance_id,ReturnRowCounts,SQLText,ParseRowCounts,QueryTimes,DBName,ExecutionStartTime,HostAddress").where("1=1  and  SQLText like '%select%'")
+    @slowlog_detail =SlowlogDetail.select("id,instance_id,ReturnRowCounts,SQLText,ParseRowCounts,QueryTimes,DBName,ExecutionStartTime,HostAddress").
+      where.not("instance_id in('rm-m5eqyh7u824r4tfho') and DBName in('ikcrm_production')").
+      where("SQLText like '%select%'")
     # "a > #{params[:b]} or "
     @slowlog_detail = @slowlog_detail.where(ExecutionStartTime: params[:sdatetime]..params[:edatetime]) if params[:sdatetime].present?
 
